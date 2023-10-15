a91c6b5835d3c5f43219bb32e6a94953a5234728
jared-finthrive <jpleva91@gmail.com>
2023-10-15 13:23:51 -0400

    Add scripts

---
+++ part_1_add_subtract.py
@@ -0,0 +1,11 @@
+# Psuedocode:
+# 1. Ask the user to input two numbers (num1 and num2).
+# 2. Given those two numbers, add them together to find the output.
+# 3. Also, subtract the two numbers to find the output.
+# 4. Print the output.
+
+if __name__ == '__main__':
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    print("Addition of two numbers is: ", num1 + num2)
+    print("Subtraction of two numbers is: ", num1 - num2)
---
+++ part_2_multiply_divide.py
@@ -0,0 +1,11 @@
+# Psuedocode:
+# 1. Ask the user to input two numbers (num1 and num2).
+# 2. Given those two numbers, multiply them together to find the output.
+# 3. Also, divide num1/num2 to find the output.
+# 4. Print the output.
+
+if __name__ == '__main__':
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    print("Multiplication of two numbers is: ", num1 * num2)
+    print("Division of two numbers is: ", num1 / num2)
